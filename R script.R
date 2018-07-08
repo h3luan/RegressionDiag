e52f6149f03f116696690a686c4dddfc222add11
h3luan <luanhui.gis@gmail.com>
2018-07-08 11:02:15 +0800

    A commit from my local computer

---
+++ R script.R
@@ -0,0 +1,17 @@
+library(car)  ###For linear regression diagnostics
+library(lmtest)
+library(caret) ##Box-Cox Transformation
+library(e1071)
+library(MASS) ##boxcox function
+
+setwd("D:/Shang Chen")
+
+data <- read.csv("dataaa.csv")
+
+y <- data$myy
+caret::BoxCoxTrans(y) ##Determine the optimum lambda for Box-Cox Transformation
+
+y_boxcox <- (y^(-0.7)-1)/(-0.7)
+
+lm_fit <- lm(formula = y_boxcox ~ cluster4 + cluster5 + cluster8 + cluster12 + cluster32 + cluster36 + cluster37 + cluster42 + cluster58, data=data)
+
---
+++ README.md
@@ -0,0 +1,1 @@
+"addiing a line" 
